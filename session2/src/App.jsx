18e7c281d0c381bf8c62d6fe9b16e35d3792ded0
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-08-17 16:05:30 +0500

    session2

---
+++ session2/src/App.jsx
@@ -0,0 +1,27 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+const Box = () => {
+  return (
+    // boxGeometry me args={[width, height, depth]} se size bara hoga
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial color="orange" wireframe={true} />
+    </mesh>
+  );
+};
+
+const App = () => {
+  return (
+    // Canvas container ki height aur width hamesha parent element se aati hai
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <Box />
+        <OrbitControls dampingFactor={0.1} />
+      </Canvas>
+    </div>
+  );
+};
+
+export default App;
