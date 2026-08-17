8f48e66fc34b7f468b6ad0b9d526a37176b10718
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-08-17 16:39:52 +0500

    3d modle add

--- session2/src/App.jsx
+++ session2/src/App.jsx
@@ -1,24 +1,25 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
-const Box = () => {
-  return (
-    // boxGeometry me args={[width, height, depth]} se size bara hoga
-    <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color="orange" wireframe={true} />
-    </mesh>
-  );
+// 1. Component name ko Capitalize kiya (Model)
+const Model = () => {
+  // 2. GLTF object se scene ko destructure kar liya
+  const { scene } = useGLTF("./modle.glb");
+
+  return <primitive object={scene} scale={0.2} position={[3, 0, 0]} />;
 };
 
 const App = () => {
   return (
-    // Canvas container ki height aur width hamesha parent element se aati hai
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <Box />
-        <OrbitControls dampingFactor={0.1} />
+      <Canvas camera={{ position: [0, 0, 5] }}>
+        <color args={["#222222"]} attach="background" />
+        <ambientLight intensity={1} />
+        <directionalLight position={[5, 5, 5]} />
+
+        {/* Capitalized component call */}
+        <Model />
+        <OrbitControls makeDefault />
       </Canvas>
     </div>
   );
