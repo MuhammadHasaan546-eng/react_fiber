319e697b4050103f3dcbd8b043fdc7c4aede16af
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-08-18 17:25:13 +0500

    gg

--- session2/src/App.jsx
+++ session2/src/App.jsx
@@ -1,35 +1,30 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
-import { OrbitControls, useHelper } from "@react-three/drei";
-import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
 
-const Scene = () => {
-  const dirLight = useRef(null);
-  useHelper(dirLight, THREE.DirectionalLightHelper, 1);
+import React from "react";
+
+const Scen = () => {
   return (
     <>
-      <directionalLight ref={dirLight} position={[0, 2, 2]} />
+      <perspectiveCamera makeDefault position={[0, 0, 5]} fov={75} />
       <mesh>
-        <boxGeometry args={[22, 2, 2]} />
-        <meshStandardMaterial
-          color={"#f04f2f"}
-          metalness={0.9}
-          roughness={0.1}
-        />
+        <boxGeometry />
+        <meshStandardMaterial />
       </mesh>
       <OrbitControls />
     </>
   );
 };
 
-export default function App() {
+const App = () => {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <Scene />
-      </Canvas>
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Scen />
+    </Canvas>
   );
-}
+};
+
+export default App;
