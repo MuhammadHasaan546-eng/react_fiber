b01fd5d1ad87001b7c2138ffc5d823fb8c04bccc
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-08-18 16:28:24 +0500

    try Hdr

--- session2/src/App.jsx
+++ session2/src/App.jsx
@@ -1,55 +1,26 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useTexture } from "@react-three/drei";
-
-const Box = () => {
-  const texture = useTexture({
-    map: "/color.jpg",
-    roughnessMap: "/tiles_0133_preview.jpg",
-    metalnessMap: "/title.png",
-  });
-
-  return (
-    // castShadow: Yeh box shadow drop karega
-    <mesh scale={1.5} position={[0, 1.5, 0]} castShadow>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial {...texture} />
-    </mesh>
-  );
-};
-
-const Plain = () => {
-  return (
-    // receiveShadow: Yeh plane doosray objects ki shadow ko capture karega
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-      <planeGeometry args={[10, 10]} />
-      <meshStandardMaterial color={"gray"} />
-    </mesh>
-  );
-};
+import { OrbitControls, Environment } from "@react-three/drei";
 
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       {/* 1. Canvas me 'shadows' prop add kiya */}
-      <Canvas shadows camera={{ position: [0, 3, 6] }}>
+      <Canvas shadows camera={{ position: [0, 0, 6] }}>
         <ambientLight intensity={0.4} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[10, 10, 10]} />
 
-        {/* 2. Directional light me shadow settings configure ki */}
-        <directionalLight
-          castShadow
-          position={[5, 8, 5]}
-          intensity={1.5}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-        />
-
-        <Suspense fallback={null}>
-          <Box />
-          <Plain />
-        </Suspense>
-
-        <OrbitControls makeDefault />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial
+            color={"#f04f2f"}
+            metalness={0.9}
+            roughness={0.1}
+          />
+        </mesh>
+        <Environment files={"/Hdri.hdr"} background />
+        <OrbitControls />
       </Canvas>
     </div>
   );
